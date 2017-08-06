9930aadd2510d841b29c1ccb37b53c13c441bc50
mao <moonjellyblue24@gmail.com>
2017-08-06 05:58:24 +0000

    top page

--- test.rb
+++ test.rb
@@ -2,5 +2,5 @@
   puts number,"\n"
 #  Task.create(status: "test status #{number}", content: "test content #{number}")
 
- Task.create(content: "task1-#{number}", status: "OnGoing #{number}")
+ Task.create(content: "test1-#{number}", status: "Test #{number}")
 end
